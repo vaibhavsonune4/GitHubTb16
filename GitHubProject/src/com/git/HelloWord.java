e57fd2ff0a1aa4b3e208d6dd3601470eceb3a2eb
vaibhavsonune4 <110394454+vaibhavsonune4@users.noreply.github.com>
2022-08-01 17:31:55 +0530

    By Cjc

--- GitHubProject/src/com/git/HelloWord.java
+++ GitHubProject/src/com/git/HelloWord.java
@@ -1,8 +1,11 @@
 package com.git;
 
 public class HelloWord {
+	public void m1()
+	{
+System.out.println("Method 1");
 
-	
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
